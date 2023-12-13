a351019e91f7602f565417e88a405fa836832742
Shawn <tomatosale7774@gmail.com>
2023-12-13 14:52:28 +0900

    Update EZStorage.swift

--- Sources/EZStorage/EZStorage.swift
+++ Sources/EZStorage/EZStorage.swift
@@ -5,16 +5,17 @@
 //  Created by 박진서 on 12/13/23.
 //
 
-import Firebase
-import FirebaseStorage
-
-open class EZStorage {
-    static private let ref = Storage.storage().reference()
-    
-    static func save(data: Data, path: String) async throws -> URL {
-        let storageRef = ref.child(path)
-        
-        let _ = try await storageRef.putDataAsync(data)
-        return try await storageRef.downloadURL()
-    }
-}
+//import FirebaseStorageCombineSwift
+//import FirebaseStorage
+//
+//open class EZStorage {
+//    static private let ref = Storage.storage().reference()
+//    
+//    static func save(data: Data, path: String) async throws -> URL {
+//        
+//        let storageRef = ref.child(path)
+//        
+//        let _ = try await storageRef.putDataAsync(data)
+//        return try await storageRef.downloadURL()
+//    }
+//}
