47654eb72ee08ae51a7f2b1ccd5f9902615dcec4
Shawn <tomatosale7774@gmail.com>
2023-12-19 14:08:52 +0900

    Update EZFirestoreType.swift

--- Sources/EZFirestore/EZFirestoreType.swift
+++ Sources/EZFirestore/EZFirestoreType.swift
@@ -22,4 +22,6 @@ protocol EZFirestoreType {
     static func fetchList<T: Codable>(of: T.Type, path: String, completion: @escaping ([T]) -> ())
     
     static func fetchWithFilter<T: Codable>(of: T.Type, path: String, filters: Filter, last: String, orderBy: String, limit: Int) async throws -> [T]
+    
+    static func search<T: Codable>(of: T.Type, path: String, keys: [String], keyword: String, last: String, orderBy: String, limit: Int) async throws -> [T]
 }
